51e2e442e96854fa27ee3576b93219864490cca8
Udishan <ishankaudishan2@gmail.com>
2024-06-08 22:38:45 +0530

    basic ui and data showing for recipe

--- src/components/common/Recipe.js
+++ src/components/common/Recipe.js
@@ -1,31 +1,53 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+
+import { TbWorldPin } from "react-icons/tb";
+import { PiBowlFoodDuotone } from "react-icons/pi";
 
 
-const Recipe = ({recipeID
-  // , strMeal, strCategory, strArea, strInstructions, strMealThumb, strTags, strYoutube
+
+const Recipe = ({ recipeID,
+  strMeal,
+  strMealThumb,
+  strArea,
+  strCategory,
+  strInstructions
+  //  strCategory, strArea, strInstructions, strMealThumb, strTags, strYoutube
 }) => {
   return (
-    <div className='h-auto w-64 bg-green-100 border border-spacing-1 border-black'>
-    {/* <Link to={`/recipe/${recipeID}`} > */}
-        <div>
-            {/* <img src={strMealThumb} alt="Description of image" className="max-h-full max-w-full object-contain" /> */}
+    <>
+      <div>
+        <div className='mt-16 mb-5'>
+          <p className='text-3xl font-bold text-center'>
+            {strMeal}
+          </p>
+        </div>
+        <div className='flex justify-center items-center'>
+          <img src={strMealThumb} alt='Meal Thumbnail' className='object-contain h-96 w-96' />
+        </div>
+        <div className='mx-16'>
+          <div className='flex h-5 items-center'>
+            <div className='mx-2'>
+              <TbWorldPin className='h-8 w-8'/>
+            </div>
+            <div className='mt-1 mx-2'>
+              <p className='text-md'>{strArea}</p>
+            </div>
+          </div>
+          <div className='flex h-5 items-center my-5'>
+            <div className='mx-2'>
+              <PiBowlFoodDuotone className='h-8 w-8'/>
+            </div>
+            <div className='mt-1 mx-2'>
+              <p className='text-md'>{strCategory}</p>
+            </div>
+          </div>
+        </div>
+        <div className='my-12 mx-28'>
+          <p className='text-justify'>{strInstructions}</p>
         </div>
 
-        <p>{recipeID}</p>
-        {/* <p>{strMeal}</p>
-        <p>{strCategory}</p>
-        <p>{strArea}</p>
-        <p>{strInstructions}</p>
-        <p>{strMealThumb}</p>
-        <p>{strTags}</p>
-        <p>{strYoutube}</p> */}
-
-        {/* <div className='mt-8 bg-green-600'>
-            <p className='text-left text-2xl mx-1'>{strMeal}</p>
-        </div> */}
-    {/* </Link> */}
-</div >
+      </div>
+    </>
   )
 }
 
--- src/components/recipeContentComponent/RecipeContent.js
+++ src/components/recipeContentComponent/RecipeContent.js
@@ -7,7 +7,7 @@ import useAxios from '../../hooks/useAxios';
 const RecipeContent = () => {
 
 
-const { recipeID } = useParams();
+  const { recipeID } = useParams();
 
   const { data, loading, error } = useAxios(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${recipeID}`);
 
@@ -16,7 +16,14 @@ const { recipeID } = useParams();
       <div>
         {console.log("id", recipeID)}
         {console.log("dasdf", data)}
-        <Recipe recipeID={data?.meals[0].idMeal}/>
+        <Recipe
+          recipeID={data?.meals[0].idMeal}
+          strMeal={data?.meals[0].strMeal}
+          strMealThumb={data?.meals[0].strMealThumb}
+          strArea={data?.meals[0].strArea}
+          strCategory={data?.meals[0].strCategory}
+          strInstructions={data?.meals[0].strInstructions}
+        />
       </div>
     </>
   )
